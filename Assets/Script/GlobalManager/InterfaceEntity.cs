0c0c3980b4d7c39e561b7f42903f39955b7fd71a
escatrgot <escatrgot@gmail.com>
2023-04-07 19:32:31 +0900

    Sophia : First Branching

--- Assets/Script/GlobalManager/InterfaceEntity.cs
+++ Assets/Script/GlobalManager/InterfaceEntity.cs
@@ -1,12 +1,21 @@
+/// <summary>
+/// 죽는것에 대한 Action
+/// </summary>
 public interface IDieAble
 {
     public void Die();
 }
 
+/// <summary>
+/// 상호작용에 대한 인터페이스
+/// </summary>
 public interface IInteractable {
     public void Interact();
 }
 
+/// <summary>
+/// 맞았을떄 대한 인터페이스
+/// </summary>
 public interface IDamagable
 {
    public void GetDamaged(int _amount);
