0aebd6f39e9b8662271449204dfff3c8b3521436
escatrgot <escatrgot@gmail.com>
2023-04-27 21:21:09 +0900

    Sophia : Update Debuff Logic

--- Assets/Script/GlobalManager/GlobalEnum.cs
+++ Assets/Script/GlobalManager/GlobalEnum.cs
@@ -2,12 +2,16 @@ public enum E_WeaponType{
     melee, ranger, mage
 }
 
+public enum E_DebuffAtomic {
+    Dot = 0, Slow, Uncontrollable
+}
+
 public enum E_DebuffState{
     Burn = 0 ,Poisend ,Bleed ,Contracted ,Slow ,Confused ,Fearing ,Stern ,Bounded    
 }
 
 public enum E_BuffState{
-
+    
 }
 
 public enum E_SynergyState{
