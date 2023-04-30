f2054cf237aa198cca4c0a4c308f80defc06b29d
escatrgot <escatrgot@gmail.com>
2023-04-30 18:43:58 +0900

    Sophia : Debuff

--- Assets/Script/Actions/Debuff/Atoms/EADA_DotDamageState.cs
+++ Assets/Script/Actions/Debuff/Atoms/EADA_DotDamageState.cs
@@ -6,32 +6,35 @@ using UnityEngine;
 
 /// <summary>
 /// Atomic한 상태이상을 가진 EntityAffector이다. 사용하기위해 이 객체의 Affect() 매서드를 호출하면 된다
-/// </br> * 이 상태이상은 플레이어를 느려지게 만드는 상태 이상이다.
 /// </summary>
 /// <param name="_owner">호출자의 게임오브젝트이다</param>
 /// <param name="_target">영향을 받는 타겟 게임오브젝트다.</param>
 /// <param name="_params">
 /// * [0]: DurationTime : float </br> 
-/// * [1]: SlowAmount : float
-/// > * 0 ~ 1 까지의 값이고, 낮을수록 느리다.
+/// * [1]: DamageAmount : int
 /// </param>
 /// <returns></returns>
-public class EADA_SlowState : EntityAffector {
+public class EADA_DotDamageState : EntityAffector {
     PlayerData mPlayerData;
     float   mDurationTime;
-    float   mSlowAmount;
-    public EADA_SlowState(GameObject _owner, GameObject _target, object[] _params) : base(_owner, _target, _params){
+    int     mDamageAmount;
+
+    public EADA_DotDamageState(GameObject _owner, GameObject _target, object[] _params) : base(_owner, _target, _params){
+        if(this.Params == null){Debug.LogError("0: 유지시간 1:도트뎀 을 적어서 보내야함");}
         _target.TryGetComponent<PlayerData>(out mPlayerData);
         mDurationTime = (float)Params[0];
-        mSlowAmount = (float)Params[1];
+        mDamageAmount = (int)Params[1];
         this.AsyncAffectorCoroutine = Coroutine();
     }
     public override void Affect() {
         this.Target.GetComponent<IAffectableEntity>().AsyncAffectHandler(this.AsyncAffectorCoroutine);
     }
     IEnumerator Coroutine() {
-        mPlayerData.numericData.MoveSpeed *= mSlowAmount;
-        yield return YieldInstructionCache.WaitForSeconds(mDurationTime);
-        mPlayerData.numericData.MoveSpeed /= mSlowAmount;
+        float passedTime = 0;
+        while(mDurationTime > passedTime){
+            passedTime += 0.5f;
+            mPlayerData.numericData.CurHP -= mDamageAmount;
+            yield return YieldInstructionCache.WaitForSeconds(0.5f);
+        }
     }
 }
