bb788fedeff8e23d779e5cabdf6772a0d3e58f64
neoskyclad <neoskyclad@naver.com>
2023-05-19 21:42:35 +0900

    상점 코드 변경 및 상점 아이템 코드 추가

--- Assets/Script/Chapter/Shop.cs
+++ Assets/Script/Chapter/Shop.cs
@@ -4,30 +4,69 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    public GameObject equipment;
-    public GameObject skill;
-    public GameObject heart;
+    public GameObject equipmentItem;
+    public GameObject skillItem;
+    public GameObject heartItem;
     public GameObject rerollMachine;
     public GameObject vendingMachine;
-    private int equipmentCount = 2;
+    private int equipmentCount = 0;
+    public int EquipmentCount{
+        get{return equipmentCount;}
+        set{equipmentCount = value;}
+    }
     private int heartCount = 0;
+    public int HeartCount{
+        get{return heartCount;}
+        set{heartCount = value;}
+    }
     private int equipmentPrice = 20;
     private int heartPrice = 10;
+    private int skillPrice = 50;
     public GameObject[] rerollable;
+    GameObject equipmentItemLocation;
+    GameObject skillItemLocation;
+    GameObject heartItemLocation;
 
     void Awake()
     {
         rerollable = new GameObject[2];
-        rerollable[0] = equipment;
-        rerollable[1] = skill;
+        rerollable[0] = equipmentItem;
+        rerollable[1] = skillItem;
+        equipmentItemLocation = transform.GetChild(0).gameObject;
+        skillItemLocation = transform.GetChild(1).gameObject;
+        heartItemLocation = transform.GetChild(2).gameObject;
     }
     void Start()
     {
-        
+        for(int i = 0; i < 3; i++)
+        {
+            InstantiateItem(i);
+        }
     }
 
     void Update()
     {
-        
+        //체력이랑 부품 리필되는 알고리즘
+    }
+    void InstantiateItem(int id)
+    {
+        GameObject temp = null;
+        if(id == 0) //equipment
+        {
+            temp = Instantiate(equipmentItem, equipmentItemLocation.transform.position, Quaternion.identity);
+            temp.GetComponent<ShopItem>().ItemPrice = equipmentPrice + equipmentCount * 5;
+        }
+        else if (id == 1)   //skill
+        {
+            temp = Instantiate(skillItem,skillItemLocation.transform.position, Quaternion.identity);
+            temp.GetComponent<ShopItem>().ItemPrice = skillPrice;
+        }
+        else if (id == 2)   //heart
+        {
+            temp = Instantiate(heartItem, heartItemLocation.transform.position, Quaternion.identity);
+            temp.GetComponent<ShopItem>().ItemPrice = heartPrice + heartCount * 5;
+            temp.GetComponent<ShopItem>().heartRecoveryRate = 5;    //하트 회복 수치
+        }
+        temp.transform.parent = transform;
     }
 }
