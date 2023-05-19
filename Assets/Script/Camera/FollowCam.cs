9f990285431556b3bc63151b6e5e0577040619c8
escatrgot <escatrgot@gmail.com>
2023-05-19 19:00:26 +0900

    Merge TA_Escatrgot

--- Assets/Script/Camera/FollowCam.cs
+++ Assets/Script/Camera/FollowCam.cs
@@ -8,12 +8,6 @@ public class FollowCam : MonoBehaviour
     public Transform chrTr;
     public Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
--- Assets/Script/Skill/Skill.cs
+++ Assets/Script/Skill/Skill.cs
@@ -4,21 +4,20 @@ using UnityEngine;
 
 public class Skill : MonoBehaviour
 {
-    [HideInInspector]
-    public PlayerData playerData;
     
     [HideInInspector]
     public WeaponData weaponData;
-    public SkillData skillData;
 
-    public GameObject skillEffect;
+    public ScriptableObjSkillData scriptableObjSkillData;
+    public static SkillData newSkillData;
 
     bool mIsReady = true;
     IEnumerator mCoWaitUse;
 
     private void Awake() {
         //if(!TryGetComponent<WeaponData>(out weaponData)) {Debug.Log("컴포넌트 로드 실패 : WeaponData");}
-        if(!TryGetComponent<SkillData>(out skillData)) {Debug.Log("컴포넌트 로드 실패 : SkillData");}
+        //if(!TryGetComponent<SkillData>(out skillData)) {Debug.Log("컴포넌트 로드 실패 : SkillData");}
+        newSkillData = new SkillData(scriptableObjSkillData);
     }
     public void Use(){
         if(!mIsReady) return;
@@ -26,7 +25,7 @@ public class Skill : MonoBehaviour
         mCoWaitUse = CoWaitUse();
         StartCoroutine(mCoWaitUse);
 
-        Instantiate(skillEffect, transform.position, transform.rotation).GetComponent<CombatEffect>().SetDatas(this.playerData, this.weaponData);
+        //Instantiate(skillEffect, transform.position, transform.rotation).GetComponent<CombatEffect>().SetDatas(this.playerData, this.weaponData);
     }
     IEnumerator CoWaitUse(){
         yield return YieldInstructionCache.WaitForSeconds(1f);
