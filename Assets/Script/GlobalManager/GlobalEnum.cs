823319130117f06ff85573be2824437c5858ee2d
escatrgot <escatrgot@gmail.com>
2023-05-29 13:28:43 +0900

    MeshTrail Update

--- Assets/Script/GlobalManager/GlobalEnum.cs
+++ Assets/Script/GlobalManager/GlobalEnum.cs
@@ -6,18 +6,9 @@ public enum E_DebuffState{
     Burn = 0 ,Poisend ,Bleed ,Contracted ,Freeze ,Confused ,Fearing ,Stern ,Bounded, Execution
 }
 
-public enum E_BuffState{
+public enum E_AffectorState {
+    //Debuff는  100  부터 시작
+    Burn = 0 ,Poisend ,Bleed ,Contracted ,Freeze ,Confused ,Fearing ,Stern ,Bounded, Execution, BlackHole
     
-}
-
-public enum E_SynergyState{
-
-}
-
-
-
-//EntityState.cs 에 또 정의함
-
-public enum Affector_PlayerState{
-    Move, Dash, Attack, Skill, GetDamaged, Die, Trigger,
+    //Buff  는    200  부터 시작
 }
