b1367363b862a16135274a7ef10f57c7938f4c49
neoskyclad <neoskyclad@naver.com>
2023-07-06 22:38:13 +0900

    상점 구현 완료, 버그: 부품 먹었을 때 0원으로 초기화

--- Assets/Script/Carrier_N_Instantiator/Equipment/ItemEquipment.cs
+++ Assets/Script/Carrier_N_Instantiator/Equipment/ItemEquipment.cs
@@ -8,19 +8,22 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Sophia_Carriers;
 
-namespace Sophia_Carriers {
-    public class ItemEquipment : Carrier {
-//      public      VFXObject       DestroyEffect       = null;
-//      public      CARRIER_TYPE    CarrierType;
-//      public      BUCKET_POSITION BucketPosition;
-//      public      bool            IsInitialized       = false;
-//      public      bool            IsActivated         = false;
-//      public      bool            IsCloned         = false;
-//      protected   Collider        carrierCollider     = null;
-//      protected   Rigidbody       carrierRigidBody    = null;
+namespace Sophia_Carriers
+{
+    public class ItemEquipment : Purchase
+    {
+        //      public      VFXObject       DestroyEffect       = null;
+        //      public      CARRIER_TYPE    CarrierType;
+        //      public      BUCKET_POSITION BucketPosition;
+        //      public      bool            IsInitialized       = false;
+        //      public      bool            IsActivated         = false;
+        //      public      bool            IsCloned         = false;
+        //      protected   Collider        carrierCollider     = null;
+        //      protected   Rigidbody       carrierRigidBody    = null;
         public AbstractEquipment equipment;
-        public override Carrier Clone(){
-            if(this.IsCloned == true) throw new SystemException("복제본이 복제본을 만들 수 는 없다.");
+        public override Carrier Clone()
+        {
+            if (this.IsCloned == true) throw new SystemException("복제본이 복제본을 만들 수 는 없다.");
             Carrier res = Instantiate(this);
             res.DisableSelf();
             res.IsCloned = true;
@@ -28,15 +31,17 @@ namespace Sophia_Carriers {
         }
 
 
-        protected override void Awake() {
+        protected override void Awake()
+        {
             base.Awake();
+            IsShopItem = false;
             this.CarrierType = CARRIER_TYPE.ITEM;
         }
 
         public override void Init(Entity _ownerEntity)
         {
-            if(IsCloned == false) throw new System.Exception("원본데이터를 조작하고 있음");
-            if(_ownerEntity == null) throw new System.Exception("투사체 생성 엔티티가 NULL임");
+            if (IsCloned == false) throw new System.Exception("원본데이터를 조작하고 있음");
+            if (_ownerEntity == null) throw new System.Exception("투사체 생성 엔티티가 NULL임");
             IsInitialized = true;
         }
         public override void InitByObject(Entity _ownerEntity, object[] _objects)
@@ -52,8 +57,16 @@ namespace Sophia_Carriers {
         //     Destroy(gameObject);
         // }
 
-        private void OnTriggerEnter(Collider other) {
-            if(!other.TryGetComponent<Player>(out Player player)){return;}
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<Player>(out Player player)) { return; }
+            if(IsShopItem)
+            {
+                if(!purchase(price))
+                    return;
+                GameManager.Instance.Shop.GetComponent<Shop>().EquipmentCount++;
+                GameManager.Instance.Shop.GetComponent<Shop>().InstantiateItem(0, 1);
+            }
             player.equipmentManager.Equip(this.equipment);
             Debug.Log($"장비 장착! : {equipment.equipmentName}");
             DestroySelf();
