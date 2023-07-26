7a89199498b79a11648b23c20d8336a73cd0cef2
neoskyclad <neoskyclad@naver.com>
2023-07-26 19:59:08 +0900

    랩터 패턴 추가

---
+++ Assets/Script/Entity/Enemy/Enemy_Template.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Template : Enemy
+{
+    public override void Die()
+    {
+        base.Die();
+        stageGenerator.CurrentMobCount--;
+        Invoke("DestroySelf", 0.5f);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
