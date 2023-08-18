57e08e2e30c837d948be065c68888f4576776499
escatrgot <escatrgot@gmail.com>
2023-08-18 12:31:37 +0900

    UI Scripting & UI 스킨 적용

---
+++ Assets/EquipmentInfoUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class EquipmentInfoUI : MonoBehaviour
+{
+    public TextMeshProUGUI  NameText;
+    public TextMeshProUGUI  AgeText;
+    public TextMeshProUGUI  StatusText;
+    public UITextList       StatsList;
+    private void OnEnable() {
+        GetData();
+    }
+    public void GetData() {
+        NameText.text = "_NameText_";
+        AgeText.text = "_AgeText_";
+        StatusText.text = "Stauts";
+        StatsList.SetAllTextListByStats();
+    }
+}
