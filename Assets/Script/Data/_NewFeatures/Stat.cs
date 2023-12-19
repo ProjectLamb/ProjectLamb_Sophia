b55d40b294cf3ec40e69253b87525477e2336c36
escatrgot <escatrgot@gmail.com>
2023-12-19 17:53:38 +0900

    ADD : 대쉬개선  대쉬 -> 스킬 판정으로수정 의의 : 페링, 장전,

--- Assets/Script/Data/_NewFeatures/Stat.cs
+++ Assets/Script/Data/_NewFeatures/Stat.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using AYellowpaper.SerializedCollections.Editor;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace Feature_NewData
@@ -23,14 +24,21 @@ namespace Feature_NewData
         public readonly E_STAT_USE_TYPE UseType;
         private float value;
         private bool isDirty = false;
+        public readonly UnityEvent OnStatChanged;
 
         private readonly List<StatCalculator> statCalculatorList = new();
 
-        public Stat(float baseValue, E_STAT_USE_TYPE UseType)
+        public Stat(float baseValue, E_STAT_USE_TYPE UseType, UnityAction statChangedHandler)
         {
             value = BaseValue = baseValue;
             this.UseType = UseType;
+            if(statChangedHandler != null) {
+                OnStatChanged = new UnityEvent();
+                OnStatChanged.AddListener(statChangedHandler);
+            }
         }
+
+        public Stat(float baseValue, E_STAT_USE_TYPE useType) : this(baseValue, useType, null){}
 
 
         public static implicit operator int(Stat stat)
@@ -56,12 +64,15 @@ namespace Feature_NewData
         // 모든것은 float로 관리하려고 했지만. MaxStamina는 무조건 Int형이 되야 한다.
         public int GetValueByNature() {
             if(UseType.Equals(E_STAT_USE_TYPE.Natural)) {
+                RecalculateStat();
                 return (int)Math.Clamp(value, 0, int.MaxValue);
             }
             throw new System.Exception("Ratio 형식을 Natural로 리턴 불가.");
         }
+
         public float GetValueByRatio() {
             if(UseType.Equals(E_STAT_USE_TYPE.Ratio)) {
+                RecalculateStat();
                 return Math.Clamp(value, 0.0f, 100);
             }
             throw new System.Exception("Natural 형식을 Ratio로 리턴 불가.");
@@ -110,6 +121,7 @@ namespace Feature_NewData
             if(value <= 1.001f) {value = 1;}
             else {value = (float) Math.Round(value, 3);}
 
+            OnStatChanged.Invoke();
             isDirty = false;
         }
     }
