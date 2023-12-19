c4fc1354be3cafc7debeb4b5377ec341ed5a6a23
escatrgot <escatrgot@gmail.com>
2023-12-19 16:16:11 +0900

    UPDATE : DashSkill Before Test

--- Assets/Script/Screen/_Feature_NewData/CoolTimeUI.cs
+++ Assets/Script/Screen/_Feature_NewData/CoolTimeUI.cs
@@ -9,16 +9,19 @@ namespace Feature_NewData
         public UnityEngine.UI.Image fill;
         public TextMeshProUGUI textMeshPro;
         
+        private int BaseStackCounts; 
         public int StackCounts; 
 
-        public CoolTimeManager coolTimeManager;
+        public CoolTimeManager coolTimeManager {get; private set;}
 
         private void Awake() {
+            BaseStackCounts = StackCounts;
             coolTimeManager = new CoolTimeManager(5f, StackCounts, true);
             coolTimeManager
                 .AddOnTickingEvent(UpdateFillAmount)
                 .AddOnUseEvent(UseStack)
-                .AddOnFinishedEvent(RecoverStack);
+                .AddOnFinishedEvent(RecoverStack)
+                .AddOnInitialized(ResetUI);
         }
 
         private void OnEnable() {
@@ -30,14 +33,41 @@ namespace Feature_NewData
             coolTimeManager.DebugStart();
         }
 
+        [ContextMenu("AccelerateFaster")] 
+        public void TestAccelerateFaster() {
+            coolTimeManager.SetAcceleratrion(2f);
+        }
+
+
+        [ContextMenu("AccelerateSlower")] 
+        public void TestAccelerateSlower() {
+            coolTimeManager.SetAcceleratrion(0.5f);
+        }
+
+
+        [ContextMenu("AccelerateReset")] 
+        public void TestAccelerateReset() {
+            coolTimeManager.SetAcceleratrion(1f);
+        }
+
+        [ContextMenu("Cool Down")] 
+        public void TestAccelerateRemainByCurrentCoolTime() {
+            coolTimeManager.AccelerateRemainByCurrentCoolTime(0.2f);
+        }
+
         private void UpdateFillAmount(float NoneUse)
         {
             fill.fillAmount = 1f - coolTimeManager.GetProgressAmount();
         }
 
+            
+        private void ResetUI() {
+            fill.fillAmount = 0;
+            StartCoroutine(DoAndRenderUI(() => {StackCounts = BaseStackCounts; textMeshPro.text = BaseStackCounts.ToString();}));
+        }
+
         IEnumerator DoAndRenderUI(UnityAction action){
-            action.Invoke();
-            yield return new WaitForEndOfFrame();
+            action.Invoke(); yield return new WaitForEndOfFrame();
         }
 
         private void UseStack() {
