45266793c25944c6cf8197f118b996e71e3c2ec3
escatrgot <escatrgot@gmail.com>
2023-12-20 11:02:32 +0900

    대쉬 UI 연결 버그 해결

--- Assets/Script/Entity/Player/Player.cs
+++ Assets/Script/Entity/Player/Player.cs
@@ -174,7 +174,9 @@ public class Player : Entity {
 
     [ContextMenu("TEST_MOD_DASHSTATS")]
     public void TEST_MOD_DASHSTATS() {
+        // 앞으로 아이템을 먹었을때 실행되는 연산이랑 동일하다.
         DashSkillAbility.MaxStamina.AddCalculator(new StatCalculator(1, E_STAT_CALC_TYPE.Add));
+        DashSkillAbility.MaxStamina.RecalculateStat();
     }
     
     public void Dash()
