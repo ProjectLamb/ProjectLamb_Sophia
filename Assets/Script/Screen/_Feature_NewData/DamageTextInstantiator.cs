dc17e327ff1d821bfa29a82077a92c75f55597f6
escatrgot <escatrgot@gmail.com>
2023-12-24 01:29:17 +0900

    FEATURE : Hit Text Effect

--- Assets/Script/Screen/_Feature_NewData/DamageTextInstantiator.cs
+++ Assets/Script/Screen/_Feature_NewData/DamageTextInstantiator.cs
@@ -2,30 +2,60 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Feature_NewData
 {
     /*********************************************************************************
-        메이플과 같은 데미지 UI를 띄울것이다.
+    메이플과 같은 데미지 UI를 띄울것이다.
 
-        그냥과 버퍼드 UI
-            AlertUIManager;
+    그냥과 버퍼드 UI
+        AlertUIManager;
 
     *********************************************************************************/
+    public enum DamageInstant_Type {
+        Stacking, Layerd
+    }
+
     public class DamageTextInstantiator : MonoBehaviour{
         
         [SerializeField] 
         private DamageTextUI textUI;
 
+        private DamageTextUI StakingText;
+        private List<DamageTextUI> LayerdTextList;
+
         public void Generate(float _damageAmount){
-            DamageTextUI InstanceOfDamageUI = Instantiate(textUI, transform)
+            if(StakingText == null) {
+                StakingText = Instantiate(textUI, transform)
                                                     .SetText(_damageAmount)
                                                     .SetAnimationSpeed(5f)
-                                                    .SetDestroyTimer(1f);
-            InstanceOfDamageUI.ActivatedTextUI();
-            
-            Destroy(InstanceOfDamageUI, InstanceOfDamageUI.DestroyTimer);
+                                                    .SetDestroyTimer(3f);
+
+                StakingText.ActivatedTextUI();
+            }
+            else {
+                StakingText.SetPosition(transform.position)
+                            .ReactivateTextUI((int)_damageAmount);
+            }
+
         }
 
+        public void Generate(List<float> _damageAmount) {
+            LayerdTextList = new List<DamageTextUI>();
+            Vector3 addingPosition = transform.position;
+            _damageAmount.ForEach((E) => {
+                LayerdTextList.Add(
+                    Instantiate(textUI, transform)
+                        .SetText(E)
+                        .SetAnimationSpeed(5f)
+                        .SetDestroyTimer(3f)
+                );
+                LayerdTextList.Last().SetPosition(addingPosition += textUI.GetNextPosition());
+            });
+            LayerdTextList.ForEach((T)=>{T.ActivatedTextUI();});
+        }
     }
 }
