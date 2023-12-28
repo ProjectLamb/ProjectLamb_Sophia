f2379154309cb8da41b7da15dc3857cfbe46d728
escatrgot <escatrgot@gmail.com>
2023-12-28 17:59:50 +0900

    UPDATE : VISUAL TEST

---
+++ Assets/Script/Data/AffectorData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Feature_NewData
+{
+    /*********************************************************************************
+    Affector 데이터를 가지고 있다.
+
+    로직타입과 비주얼 타입을 가지고 있다.
+
+    
+    *********************************************************************************/
+    public class AffectorData {
+
+    }
+}
