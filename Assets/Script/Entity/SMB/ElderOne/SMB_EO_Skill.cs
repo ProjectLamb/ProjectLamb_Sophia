30869f85902c9fedb7b83de1acd3bda9e334d67f
neoskyclad <neoskyclad@naver.com>
2024-01-21 03:07:16 +0900

    [feat][fix] BT 삭제, FSM 라이브러리 추가, 엘더원 FSM 패턴으로 교체

--- Assets/Script/Entity/SMB/ElderOne/SMB_EO_Skill.cs
+++ Assets/Script/Entity/SMB/ElderOne/SMB_EO_Skill.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FSM_EO_AttackWalkEnd : StateMachineBehaviour
+public class SMB_EO_Skill : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetBool("IsAttackWalkEnd", true);
+        animator.SetInteger("attackCount", 0);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -17,10 +17,10 @@ public class FSM_EO_AttackWalkEnd : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        animator.SetBool("IsAttackWalkEnd", false);
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //   animator.SetBool("IsSkill", false);
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
