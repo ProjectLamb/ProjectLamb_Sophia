63768c43998990215b67293b6bf2b1564f3d932e
escatrgot <escatrgot@gmail.com>
2024-01-23 01:39:19 +0900

    [update] : before merge

--- Assets/Script/Data/EntityData.cs
+++ Assets/Script/Data/EntityData.cs
@@ -28,9 +28,9 @@ public struct EntityData
     public UnityAction  HitState;
     public UnityActionRef<float> HitStateRef;
     public UnityAction<Entity, Entity> ProjectileShootState;
-    public UnityAction  PhyiscTriggerState;
-    public UnityAction  DieState;
-    public UnityAction  UIAffectState;
+    public UnityAction PhyiscTriggerState;
+    public UnityAction DieState;
+    public UnityAction UIAffectState;
     public EntityData(ScriptableObjEntityData _scriptable)
     {
         EntityTag = _scriptable.EntityTag;
@@ -54,8 +54,8 @@ public struct EntityData
     public static EntityData operator +(EntityData x, EntityData y)
     {
         EntityData res = new EntityData();
-        if(!String.IsNullOrEmpty(x.EntityTag)){res.EntityTag = x.EntityTag;}
-        if(!String.IsNullOrEmpty(y.EntityTag)){res.EntityTag = y.EntityTag;}
+        if (!String.IsNullOrEmpty(x.EntityTag)) { res.EntityTag = x.EntityTag; }
+        if (!String.IsNullOrEmpty(y.EntityTag)) { res.EntityTag = y.EntityTag; }
         res.MaxHP = x.MaxHP + y.MaxHP;
         res.MoveSpeed = x.MoveSpeed + y.MoveSpeed;
         res.Defence = x.Defence + y.Defence;
@@ -77,8 +77,8 @@ public struct EntityData
     public static EntityData operator -(EntityData x, EntityData y)
     {
         EntityData res = new EntityData();
-        if(!String.IsNullOrEmpty(x.EntityTag)){res.EntityTag = x.EntityTag;}
-        if(!String.IsNullOrEmpty(y.EntityTag)){res.EntityTag = y.EntityTag;}
+        if (!String.IsNullOrEmpty(x.EntityTag)) { res.EntityTag = x.EntityTag; }
+        if (!String.IsNullOrEmpty(y.EntityTag)) { res.EntityTag = y.EntityTag; }
         res.MaxHP = x.MaxHP - y.MaxHP;
         res.MoveSpeed = x.MoveSpeed - y.MoveSpeed;
         res.Defence = x.Defence - y.Defence;
