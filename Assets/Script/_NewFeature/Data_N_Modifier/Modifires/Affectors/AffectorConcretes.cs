85f22e15191b8873aba0df5303482220cad33ce7
escatrgot <escatrgot@gmail.com>
2024-02-02 02:03:32 +0900

    [feat] : Airborn Affect 제작

--- Assets/Script/_NewFeature/Data_N_Modifier/Modifires/Affectors/AffectorConcretes.cs
+++ Assets/Script/_NewFeature/Data_N_Modifier/Modifires/Affectors/AffectorConcretes.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 
 namespace Sophia.DataSystem
 {
@@ -10,11 +11,11 @@ namespace Sophia.DataSystem
         using Sophia.Instantiates;
         using Sophia.Entitys;
         using System.Threading;
-
-
+        using Unity.VisualScripting;
+        using System.Numerics;
 
         public class PoisonedAffect : Affector {
-    #region Members
+#region Members
 //          public readonly E_AFFECT_TYPE AffectType;
 //          public readonly Entity TargetRef;
 //          public readonly Entity ownerEntity;
@@ -39,9 +40,9 @@ namespace Sophia.DataSystem
                 Timer.OnFinished += Revert;
             }
 
-    #endregion
+#endregion
 
-    #region Setter
+#region Setter
 
             public PoisonedAffect SetMaterial(Material material) {
                 this.materialRef = material;
@@ -79,9 +80,9 @@ namespace Sophia.DataSystem
                 return this;
             }
             
-    #endregion
+#endregion
     
-    #region Modifiy
+#region Modifiy
                     
             public override void Modifiy(float tenacity)
             {
@@ -107,9 +108,9 @@ namespace Sophia.DataSystem
                 base.CancleModify();
             }
 
-    #endregion
+#endregion
 
-    #region Affects
+#region Affects
 
             private void PoisonedDamage() =>  TargetRef.GetDamaged((int)CalcTickDamage());
             private async void PoisonedMeshOn()    {
@@ -141,14 +142,174 @@ namespace Sophia.DataSystem
                 if(visualFxRef.DEBUG)Debug.Log($"{visualFxRef.gameObject.name} 지금 타입은 {visualFxRef.AffectType.ToString()}");
                 TargetRef.GetVisualFXBucket().RemoveInstantableFromBucket(this.AffectType);
             }
-    #endregion
+#endregion
 
-    #region Helper
+#region Helper
 
             private float CalcTickDamage() => TickDamageRatio * TickDamage;
             private float CalcDurateTime(float tenacity) => BaseDurateTime * (1-tenacity);
 
-            #endregion
+#endregion
+        }
+        public class SternAffect : Affector {
+#region Members
+            private CancellationTokenSource cts;
+            public Material materialRef {get; private set;}
+            public VisualFXObject visualFxRef {get; private set;}
+
+            public SternAffect(Entity ownerReceivers, Entity targetReceivers, float durateTime) 
+            : base(E_AFFECT_TYPE.Stern, ownerReceivers, targetReceivers, durateTime) {
+                cts = new CancellationTokenSource();
+                targetReceivers.GetLifeComposite().OnExitDie += () => cts.Cancel();
+                Timer.OnFinished += Revert;
+
+                Timer.OnStart += FreezeMovementOn;
+                Timer.OnFinished += FreezeMovementOff;
+            }
+            
+#endregion
+#region Setter
+            public SternAffect SetMaterial(Material material) {
+                this.materialRef = material;
+                Timer.OnStart       += SternMeshOn;
+                Timer.OnFinished    += SternMeshOff;
+                return this;
+            }
+            public SternAffect SetVisualFXObject(VisualFXObject vfx) {
+                this.visualFxRef = vfx;
+                Timer.OnStart       += SternVFXOn;
+                Timer.OnFinished    += SternVFXOff;
+                return this;
+            }
+#endregion
+#region Modifiy
+
+            public override void Modifiy(float tenacity)
+            {
+                Timer.SetStart();
+                Timer.Execute();
+                base.Modifiy(tenacity);
+            }
+
+            public override void TickRunning() {
+                Timer.Execute();
+                base.TickRunning();
+            }
+            
+            public override void Revert()
+            {
+                base.Revert();
+            }
+
+            public override void CancleModify()
+            {
+                Timer.Pause();
+                Timer.ChangeState(TimerExit.Instance);
+                base.CancleModify();
+            }
+
+#endregion
+#region Affects
+            private void FreezeMovementOn() {
+                if(TargetRef is IMovable) {
+                    IMovable movableEntity = TargetRef as IMovable;
+                    movableEntity.SetMoveState(false);
+                }
+            }
+            private void FreezeMovementOff() {
+                if(TargetRef is IMovable) {
+                    IMovable movableEntity = TargetRef as IMovable;
+                    movableEntity.SetMoveState(true);
+                }
+            }
+
+            private async void SternMeshOn()    {
+                try {
+                    if(TargetRef == null) return;
+                    await TargetRef.GetModelManger().ChangeSkin(cts.Token, materialRef);
+                }   
+                catch(OperationCanceledException){}             
+            }
+            private async void SternMeshOff()   {
+                try {
+                    if(TargetRef == null) return;
+                    await TargetRef.GetModelManger().RevertSkin(cts.Token);
+                }
+                catch(OperationCanceledException){}
+            }
+            private void SternVFXOn()  {
+                VisualFXObject visualFX = VisualFXObjectPool.GetObject(visualFxRef, OwnerRef);
+                TargetRef.GetVisualFXBucket().InstantablePositioning(visualFX).Activate();
+            }
+            private void SternVFXOff() {
+                TargetRef.GetVisualFXBucket().RemoveInstantableFromBucket(E_AFFECT_TYPE.Stern);
+            }
+#endregion
+#region Helper
+
+#endregion
+        }
+        public class AirborneAffect : Affector {
+#region Members
+            public AirborneAffect(Entity ownerReceivers, Entity targetReceivers, float durateTime) 
+            : base(E_AFFECT_TYPE.Airborn, ownerReceivers, targetReceivers, durateTime) {
+                Timer.OnFinished += Revert;
+                Timer.OnStart += AirboanStartOn;
+                Timer.OnFinished += AirboanMovementOff;
+            }
+            
+#endregion
+#region Setter
+
+#endregion
+#region Modifiy
+
+            public override void Modifiy(float tenacity)
+            {
+                Timer.SetStart();
+                Timer.Execute();
+                base.Modifiy(tenacity);
+            }
+
+            public override void TickRunning() {
+                Timer.Execute();
+                base.TickRunning();
+            }
+            
+            public override void Revert()
+            {
+                base.Revert();
+            }
+
+            public override void CancleModify()
+            {
+                Timer.Pause();
+                Timer.ChangeState(TimerExit.Instance);
+                base.CancleModify();
+            }
+
+#endregion
+#region Affects
+            private void AirboanStartOn() {
+                if(TargetRef is IMovable) {
+                    IMovable movableEntity = TargetRef as IMovable;
+                    
+                    GameObject targetModel = TargetRef.GetModelManger().GetModelObject();
+                    targetModel.transform.DOLocalJump(UnityEngine.Vector3.zero, 1, 1, BaseDurateTime);
+                    movableEntity.SetMoveState(false);
+                }
+            }
+            private void AirboanMovementOff() {
+                if(TargetRef is IMovable) {
+                    IMovable movableEntity = TargetRef as IMovable;
+                    movableEntity.SetMoveState(true);
+                }
+            }
+
+#endregion
+#region Helper
+
+#endregion
         }
     }
 }
