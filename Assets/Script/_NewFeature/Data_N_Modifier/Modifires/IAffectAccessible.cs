f663895956a6fdd43ecfb28d5932f9873122a045
escatrgot <escatrgot@gmail.com>
2024-02-02 02:09:08 +0900

    [fix] : 철자 수정 에휴 ㅋㅋ;

--- Assets/Script/_NewFeature/Data_N_Modifier/Modifires/IAffectAccessible.cs
+++ Assets/Script/_NewFeature/Data_N_Modifier/Modifires/IAffectAccessible.cs
@@ -9,7 +9,7 @@ namespace Sophia
         // 블랙홀
         Debuff = 100,
         Burn, Poisoned, Bleed, Contracted, Freeze, Confused, Fear, Stern, Bounded, Execution,
-        BlackHole, Airborn,
+        BlackHole, Airborne,
 
         // 이동속도증가, 고유시간가속, 공격력증가, 보호막상태, CC저항, 은신, 무적, 방어/페링, 투사체생성, 회피,
         Buff = 200,
