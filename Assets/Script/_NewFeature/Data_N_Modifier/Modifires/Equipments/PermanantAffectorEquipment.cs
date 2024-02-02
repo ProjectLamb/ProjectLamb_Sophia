83de8bf99149f5e3b3d7bba1c92557a04039c4a1
escatrgot <escatrgot@gmail.com>
2024-02-02 17:48:33 +0900

    [feat] : Advanced Debuff System Refactored

--- Assets/Script/_NewFeature/Data_N_Modifier/Modifires/Equipments/PermanantAffectorEquipment.cs
+++ Assets/Script/_NewFeature/Data_N_Modifier/Modifires/Equipments/PermanantAffectorEquipment.cs
@@ -2,31 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-using Random = UnityEngine.Random;
 using Sophia;
 using Sophia.DataSystem.Modifiers.ConcreteAffectors;
 using Sophia.DataSystem.Modifiers.Affector;
 using UnityEngine.UIElements;
-using Sophia.DataSystem.Modifiers;
 using Sophia.DataSystem;
 using Sophia.Instantiates;
+using Sophia.DataSystem.Functional;
+using Sophia.DataSystem.Modifiers;
+using Sophia.DataSystem.Numerics;
 
-public class PermanantAffectorEquipment : Carrier { //, IPlayerDataApplicant{
+public class PermanantAffectorEquipment : Carrier, IUserInterfaceAccessible
+{ //, IPlayerDataApplicant{
+    [SerializeField] public string _equipmentName;
+    [SerializeField] public Sprite _icon;
+    [SerializeField] public string _description;
+
+    [SerializeField] public E_AFFECT_TYPE _affectType;
+    [SerializeField] public Material _material;
+    [SerializeField] public Sophia.Instantiates.VisualFXObject _visualFx;
+    [SerializeField] public float _baseDurateTime;
+    [SerializeField] public float _intervalTime;
+    [SerializeField] public float _tickDamageRatio;
+    [SerializeField] private float _tickDamage;
+    [SerializeField] SerialCalculateDatas _calculateDatas;
 
     ExtrasModifier<Sophia.Entitys.Entity> extrasModifier;
     Sophia.Entitys.Entity OwnerRef;
+    IFunctionalCommand<Sophia.Entitys.Entity> affectCommand;
+    private void Awake()
+    {
 
-    private void Awake() {
-        
         /******************************************************
 
         Extras<Sophia.Entitys.Entity> TargetAffectedExtrasRef
             = OwnerRef.GetExtras<Sophia.Entitys.Entity>(E_FUNCTIONAL_EXTRAS_TYPE.TargetAffected);
         
         ******************************************************/
-        
+
         /******************************************************
         
         ExtrasModifier<Sophia.Entitys.Entity> extrasModifier 
@@ -41,55 +55,93 @@ public class PermanantAffectorEquipment : Carrier { //, IPlayerDataApplicant{
         TargetAffectedExtrasRef.AddModifier(this.extrasModifier);
         
         ******************************************************/
-        UnityActionRef<Sophia.Entitys.Entity> affectAction = null;
-        switch(_affectType) {
-            case E_AFFECT_TYPE.Poisoned : {affectAction = OnPoisionAffectedToTarget; break;}
-            case E_AFFECT_TYPE.Stern    : {affectAction = OnSternAffectedToTarget; break;}
-            case E_AFFECT_TYPE.Airborne    : {affectAction = OnAirbornAffectedToTarget; break;}
-            default : {throw new System.Exception("현재 알맞는 어펙터가 없음");}
+        switch (_affectType)
+        {
+            case E_AFFECT_TYPE.Poisoned:
+                {
+                    affectCommand = new PoisionAffectConveyerCommand(
+                        this.OwnerRef,
+                        _material, _visualFx,
+                        _baseDurateTime, _intervalTime, _tickDamage, _tickDamageRatio
+                    );
+                    break;
+                }
+            case E_AFFECT_TYPE.Stern:
+                {
+                    affectCommand = new SternAffectConveyerCommand(
+                        this.OwnerRef,
+                        _material, _visualFx,
+                        _baseDurateTime, _intervalTime, _tickDamage, _tickDamageRatio
+                    );
+                    break;
+                }
+            case E_AFFECT_TYPE.Airborne:
+                {
+                    affectCommand = new AirborneAffectConveyerCommand(
+                        this.OwnerRef,
+                        _material, _visualFx,
+                        _baseDurateTime, _intervalTime, _tickDamage, _tickDamageRatio,
+                        3
+                    );
+                    break;
+                }
+            case E_AFFECT_TYPE.Frozen:
+                {
+                    affectCommand = new FrozenAffectConveyerCommand(
+                        this.OwnerRef,
+                        _material, _visualFx,
+                        _baseDurateTime, _intervalTime, _tickDamage, _tickDamageRatio,
+                        _calculateDatas
+                    );
+                    break;
+                }
+            case E_AFFECT_TYPE.BlackHole :
+                {
+                    affectCommand = new BlackHoleAffectConveyerCommand(
+                        this.OwnerRef,
+                        null, null,
+                        _baseDurateTime, _intervalTime, -1, -1,
+                        50
+                    );
+                    break;
+                }
+            default: { throw new System.Exception("현재 알맞는 어펙터가 없음"); }
         }
 
-        extrasModifier = new ExtrasModifier<Sophia.Entitys.Entity>(affectAction, E_EXTRAS_PERFORM_TYPE.Tick, E_FUNCTIONAL_EXTRAS_TYPE.TargetAffected);
-    }
-    [SerializeField] public E_AFFECT_TYPE _affectType;
-    [SerializeField] public Material _material;
-    [SerializeField] public Sophia.Instantiates.VisualFXObject _visualFx;
-    [SerializeField] public float _baseDurateTime;
-    [SerializeField] public float _intervalTime;
-    [SerializeField] public float _TickDamageRatio;
-    [SerializeField] private float _TickDamage;
-    public void OnPoisionAffectedToTarget(ref Sophia.Entitys.Entity target) {
-        Affector PoisionAffector = new PoisonedAffect(this.OwnerRef, target, this._baseDurateTime)
-                                        .SetIntervalTime(this._intervalTime)
-                                        .SetTickDamage(this._TickDamage)
-                                        .SetTickDamageRatio(this._TickDamageRatio)
-                                        .SetMaterial(this._material)
-                                        .SetVisualFXObject(this._visualFx);
-        PoisionAffector.ConveyToTarget();
-    }
-
-    public void OnSternAffectedToTarget(ref Sophia.Entitys.Entity target) {
-        Affector SternAffector = new SternAffect(this.OwnerRef, target, this._baseDurateTime)
-                                        .SetMaterial(this._material)
-                                        .SetVisualFXObject(this._visualFx);
-        SternAffector.ConveyToTarget();
-    }
-    public void OnAirbornAffectedToTarget(ref Sophia.Entitys.Entity target) {
-        Affector SternAffector = new AirborneAffect(this.OwnerRef, target, this._baseDurateTime);
-        SternAffector.ConveyToTarget();
+        extrasModifier = new ExtrasModifier<Sophia.Entitys.Entity>(affectCommand, E_EXTRAS_PERFORM_TYPE.Tick, E_FUNCTIONAL_EXTRAS_TYPE.TargetAffected);
     }
 
     protected override void OnTriggerLogic(Collider entity)
     {
-        if(entity.TryGetComponent<Sophia.Entitys.Player>(out Sophia.Entitys.Player player)){
+        if (entity.TryGetComponent(out Sophia.Entitys.Player player))
+        {
             Debug.Log("Triggerd");
-            OwnerRef = player;
+            this.OwnerRef = player;
             Extras<Sophia.Entitys.Entity> extrasRef = OwnerRef.GetExtras<Sophia.Entitys.Entity>(E_FUNCTIONAL_EXTRAS_TYPE.TargetAffected);
             extrasRef.AddModifier(this.extrasModifier);
+            (extrasModifier.Functional as AffectConveyerCommand).SetOwner(player);
+            Debug.Log($"아이템 이름 : {GetName()} 효과 : {GetDescription()}");
             Destroy(this.gameObject);
         }
-        else {
-            Debug.Log("entity는 Player가 아니거나 못찾음" );
+        else
+        {
+            Debug.Log("entity는 Player가 아니거나 못찾음");
         }
+    }
+
+    public string GetName()
+    {
+        return _equipmentName;
+    }
+
+    public string GetDescription()
+    {
+        string res = $"{_description}\n {affectCommand.GetName()} : {affectCommand.GetDescription()}";
+        return res;
+    }
+
+    public Sprite GetSprite()
+    {
+        return _icon;
     }
 }
