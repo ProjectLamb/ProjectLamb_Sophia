6a35a47c16caeef956960d02c0bd73e0b9762ffa
escatrgot <escatrgot@gmail.com>
2024-02-04 16:23:05 +0900

    [feat] : 기획 디버프 문서를 참고하여 디버프 구현과 명명을 수정

--- Assets/Script/_NewFeature/Data_N_Modifier/Modifires/IAffectAccessible.cs
+++ Assets/Script/_NewFeature/Data_N_Modifier/Modifires/IAffectAccessible.cs
@@ -8,7 +8,7 @@ namespace Sophia
         // 화상, 독, 출혈, 수축, 냉기, 혼란, 공포, 스턴, 속박, 처형
         // 블랙홀
         Debuff = 100,
-        Burn, Poisoned, Bleed, Contracted, Frozen, Confused, Fear, Stern, Bounded, Execution,
+        Burn, Poisoned, Bleed, Contracted, Cold, Confused, Fear, Stern, Bounded, 
         Knockback, BlackHole, Airborne, 
 
         // 이동속도증가, 고유시간가속, 공격력증가, 보호막상태, CC저항, 은신, 무적, 방어/페링, 투사체생성, 회피,
