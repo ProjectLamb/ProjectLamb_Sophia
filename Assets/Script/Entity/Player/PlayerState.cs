5a6498ccc197fe759d1755ebed97db84ec560552
HobakDG <cil1216@naver.com>
2024-02-13 12:58:27 +0900

    [feat][docs] 플레이어 FSM 기본적인 구조 작성

--- Assets/Script/Entity/Player/PlayerState.cs
+++ Assets/Script/Entity/Player/PlayerState.cs
@@ -2,9 +2,101 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface PlayerState<T>
+namespace PlayerState
 {
-    void StateEnter(T sender);
-    void StateUpdate(T sender);
-    void StateExit(T sender);
+    // 플레이어 기본 상태(키 입력 X일때)
+    public class Idle : State
+    {
+        public override void Enter(Player player)
+        {
+
+        }
+
+        public override void Update(Player player)
+        {
+            Debug.Log("현재 idle 상태");
+        }
+
+        public override void Exit(Player player)
+        {
+
+        }
+    }
+
+    // 플레이어 이동 상태
+    public class Move : State
+    {
+        public override void Enter(Player player)
+        {
+
+        }
+
+        public override void Update(Player player)
+        {
+
+        }
+
+        public override void Exit(Player player)
+        {
+            
+        }
+    }
+
+    // 플레이어 공격 상태
+    public class Attack : State
+    {
+        public override void Enter(Player player)
+        {
+            Debug.Log("attack state 진입");
+        }
+
+        public override void Update(Player player)
+        {
+
+        }
+
+        public override void Exit(Player player)
+        {
+   
+        }
+    }
+
+    // 플레이어 피격(공격받음) 상태
+    public class GetDamaged : State
+    {
+        public override void Enter(Player player)
+        {
+
+        }
+
+        public override void Update(Player player)
+        {
+
+        }
+
+        public override void Exit(Player player)
+        {
+            
+        }
+    }
+
+    // 플레이어 사망 상태
+    public class Die : State
+    {
+        public override void Enter(Player player)
+        {
+
+        }
+
+        public override void Update(Player player)
+        {
+
+        }
+
+        public override void Exit(Player player)
+        {
+            
+        }
+    }
+
 }
