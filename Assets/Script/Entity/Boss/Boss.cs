db30cf6a380395ae7c79f9b9059b367fc392ada1
escatrgot <escatrgot@gmail.com>
2024-02-18 15:48:25 +0900

    [feat] : RecognizeEntityComposite FieldOfView  기능 확장

--- Assets/Script/Entity/Boss/Boss.cs
+++ Assets/Script/Entity/Boss/Boss.cs
@@ -5,13 +5,24 @@ using Sophia_Carriers;
 
 public class Boss : Enemy
 {
-    protected FieldOfView fov;
+
+#region SerailizeMemver
+
+    [SerializeField] protected Sophia.Composite.RecognizeEntityComposite recognize;
+    [SerializeField] protected Sophia.Composite.SerialFieldOfViewData serialFieldOfViewData;
+
+#endregion
+
+#region Member
+
     protected int phase = 1;
+
+#endregion
 
     protected override void Awake()
     {
         base.Awake();
-        TryGetComponent<FieldOfView>(out fov);
+        recognize = new Sophia.Composite.RecognizeEntityComposite(this.gameObject, this.serialFieldOfViewData);
     }
     void Start()
     {
--- Assets/Script/_NewFeature/CompositionClasses/RecognizeEntityComposite.cs
+++ Assets/Script/_NewFeature/CompositionClasses/RecognizeEntityComposite.cs
@@ -23,21 +23,37 @@ namespace Sophia.Composite
         [SerializeField] public LayerMask targetMask, obstacleMask;
     }
 
+    public interface IRecogStateAccessible {
+        public RecognizeEntityComposite GetRecognizeComposite();
+    }
+
     public class RecognizeEntityComposite {
         private E_RECOG_TYPE RecogType;
         private FieldOfView FOV;
+        private bool RecogOnce;
         public RecognizeEntityComposite(GameObject gameObject, in SerialFieldOfViewData serialFieldOfViewData) {
             FOV = gameObject.AddComponent<FieldOfView>();
             FOV.viewAngle       =  serialFieldOfViewData.viewAngle;
             FOV.viewRadius      =  serialFieldOfViewData.viewRadius;
             FOV.targetMask      =  serialFieldOfViewData.targetMask;
             FOV.obstacleMask    =  serialFieldOfViewData.obstacleMask;
+            FOV.enabled = true;
             RecogType = E_RECOG_TYPE.None;
         }
 
         public float GetViewRadius() => FOV.viewRadius;
         public float GetViewAngle() => FOV.viewAngle;
-        public E_RECOG_TYPE GetCurrentRecogState()=> RecogType;
+        public E_RECOG_TYPE GetCurrentRecogState() {
+            if(FOV.IsRecog)
+                if(!RecogOnce){
+                    RecogOnce = true;
+                    return RecogType = E_RECOG_TYPE.FirstRecog;
+                }
+                else 
+                    return RecogType = E_RECOG_TYPE.ReRecog;
+            else 
+                return RecogType = E_RECOG_TYPE.Lose;
+        }
 
         public List<Entity> GetEntities() {
             List<Entity> entities = new List<Entity>();
