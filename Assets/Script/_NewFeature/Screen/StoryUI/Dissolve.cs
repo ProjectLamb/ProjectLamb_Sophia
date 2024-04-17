d20fb4e54e644bbbba7968ceec656d4243681496
HobakDG <cil1216@naver.com>
2024-04-18 03:32:35 +0900

    [doc] 불필요한 주석 제거

--- Assets/Script/_NewFeature/Screen/StoryUI/Dissolve.cs
+++ Assets/Script/_NewFeature/Screen/StoryUI/Dissolve.cs
@@ -5,34 +5,34 @@ using UnityEngine.UI;
 
 public class Dissolve : MonoBehaviour
 {
-    GameObject dissolvePanel;               //판넬오브젝트
-    RawImage rwImage;                            //판넬 이미지
-    private bool IsDissolveOver = false;     //투명도 조절 논리형 변수
+    GameObject dissolvePanel;       
+    RawImage rwImage;                    
+    private bool IsDissolveOver = false;    
 
     void Awake()
     {
-        dissolvePanel = this.gameObject;                         //스크립트 참조된 오브젝트
-        rwImage = dissolvePanel.GetComponent<RawImage>();    //판넬오브젝트에 이미지 참조
+        dissolvePanel = this.gameObject;                        
+        rwImage = dissolvePanel.GetComponent<RawImage>();    
     }
     void Update()
     {
-        StartCoroutine("DissolveEffect");                        //코루틴    //판넬 투명도 조절
-        if (IsDissolveOver)                                            //만약 IsDissolveOver 이 참이면
+        StartCoroutine("DissolveEffect");                     
+        if (IsDissolveOver)                                
         {
-            Destroy(this.gameObject);                        //판넬 파괴, 삭제
+            Destroy(this.gameObject);       
         }
     }
     IEnumerator DissolveEffect()
     {
-        Color color = rwImage.color;                            //color 에 판넬 이미지 참조
-        color.a -= Time.deltaTime * 0.075f;               //이미지 알파 값을 타임 델타 값 * 0.01
+        Color color = rwImage.color;                     
+        color.a -= Time.deltaTime * 0.075f;             
         rwImage.color = color;    
-                                    //판넬 이미지 컬러에 바뀐 알파값 참조
-        if (rwImage.color.a <= 0)                        //만약 판넬 이미지 알파 값이 0보다 작으면
+                                    
+        if (rwImage.color.a <= 0)                   
         {
-            IsDissolveOver = true;                              //IsDissolveOver 참 
+            IsDissolveOver = true;              
         }
-        yield return null;                                        //코루틴 종료
+        yield return null;                           
 
     }
 }
