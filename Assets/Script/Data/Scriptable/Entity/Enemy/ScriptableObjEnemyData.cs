23d9cca0d6a47ef28649a37453171818f708e842
DogGuyMan <escatrgot@gmail.com>
2024-11-25 06:15:13 +0900

    [Feat] : Scriptable Remove From Data

--- Assets/Script/Data/Scriptable/Entity/Enemy/ScriptableObjEnemyData.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-using UnityEngine.Events;
-
-[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObject/Enemy", order = int.MaxValue)]
-public class ScriptableObjEnemyData : ScriptableObjEntityData {
-}
